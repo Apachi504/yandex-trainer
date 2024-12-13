644ae2b7aed5e6eb50996ed7b367ee865a978fd2
Apachi504 <apachi504@gmail.com>
2024-12-13 03:19:48 +0300

    Урок 4: Инициализация хранилища. Первый reducer и action 2/2

--- src/services/reducers/index.js
+++ src/services/reducers/index.js
@@ -1,6 +1,33 @@
 import {cartReducer} from "./cart.js";
 import {combineReducers} from "redux";
+import {PREVIOUS_STEP, NEXT_STEP} from "../actions/index.js";
 
-export const rootReducer = combineReducers  ({
-    cart: cartReducer
+const stepReducer = (state = 'cart', action) => {
+    switch (action.type) {
+        case NEXT_STEP: {
+            return state === 'cart'
+                ? 'delivery'
+                : state === 'delivery'
+                ? 'checkout'
+                : state === 'checkout'
+                ? 'checkout'
+                : 'checkout';
+        }
+        case PREVIOUS_STEP: {
+            return state ==='cart'
+            ? 'cart'
+            : state === 'delivery'
+            ?'cart'
+            :state ==='checkout'
+            ?'delivery'
+            :'cart';
+        }
+        default: {
+            return state;
+        }
+    }
+}
+export const rootReducer = combineReducers({
+    cart: cartReducer,
+    step: stepReducer,
 });
